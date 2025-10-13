8f35c02261e3d56d2b635e267d3eb62097312d87
Sandra120704 <sandra@example.com>
2025-10-13 16:13:13 -0500

    cambios realizado

--- config/database.php
+++ config/database.php
@@ -1,13 +1,11 @@
 <?php
-// config/database.php
-
 $isAzure = isset($_SERVER['WEBSITE_SITE_NAME']);
 
 if ($isAzure) {
-    $host = getenv('DB_HOST');
-    $dbname = getenv('DB_NAME');
-    $username = getenv('DB_USER');
-    $password = getenv('DB_PASS');
+    $host = getenv('AZURE_MYSQL_HOST');
+    $dbname = getenv('AZURE_MYSQL_DBNAME');
+    $username = getenv('AZURE_MYSQL_USERNAME');
+    $password = getenv('AZURE_MYSQL_PASSWORD');
 } else {
     $host = 'localhost';
     $dbname = 'biblioteca_senati';
@@ -18,7 +16,6 @@ if ($isAzure) {
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 } catch(PDOException $e) {
     die("Error de conexión: " . $e->getMessage());
 }
